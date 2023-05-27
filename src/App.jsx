84e9820a6c3c21e7a612c3825daf1c37e8676313
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-05-27 18:39:15 -0500

    Add routes to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Home from './components/home/home';
+import Header from './components/header/header';
 
 function App() {
   return (
-    <main>
-      <h1>My Portfolio</h1>
-    </main>
+    <div className="App">
+
+      <BrowserRouter>
+
+        <Header />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<Home />} />
+            <Route path="/projects" element={<Home />} />
+            <Route path="/contact" element={<Home />} />
+          </Routes>
+        </main>
+
+      </BrowserRouter>
+
+    </div>
   );
 }
 
