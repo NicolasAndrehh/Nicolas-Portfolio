4dbc832b59e7f8243d88027a7c67c753d9ef6ec1
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-06-02 17:58:37 -0500

    Add header transition on scroll

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,43 +1,66 @@
+import {
+  faBriefcase, faEnvelope,
+  faHouse, faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faHouse, faUser, faBriefcase, faEnvelope,
-} from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => (
-  <header className="header">
-    <h1>Welcome!</h1>
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
 
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li className="nav-item">
-          <NavLink to="/" activeClassName="active" end>
-            <FontAwesomeIcon icon={faHouse} className="icon" />
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/about" activeClassName="active">
-            <FontAwesomeIcon icon={faUser} className="icon" />
-            About
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/projects" activeClassName="active">
-            <FontAwesomeIcon icon={faBriefcase} className="icon" />
-            Projects
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/contact" activeClassName="active">
-            <FontAwesomeIcon icon={faEnvelope} className="icon" />
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      console.log(position, scrolled);
+      if (position > 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={scrolled ? 'header scrolled' : 'header'}>
+      <h1>Welcome!</h1>
+
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li className="nav-item">
+            <NavLink to="/" end>
+              <FontAwesomeIcon icon={faHouse} className="icon" />
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about">
+              <FontAwesomeIcon icon={faUser} className="icon" />
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/projects">
+              <FontAwesomeIcon icon={faBriefcase} className="icon" />
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/contact">
+              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
