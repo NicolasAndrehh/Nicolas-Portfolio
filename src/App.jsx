92ef8e503be764d1c1fae001d90d284ac19a8dcc
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-08-04 17:38:13 -0500

    Fix scroll behavior on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import {
+  Route, Routes, useLocation,
+} from 'react-router-dom';
 import './App.scss';
 import About from './components/about/about';
+import Contact from './components/contact/contact';
 import Header from './components/header/header';
 import Home from './components/home/home';
+import Projects from './components/projects/projects';
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="App">
 
-      <BrowserRouter>
+      <Header />
 
-        <Header />
-
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Home />} />
-            <Route path="/contact" element={<Home />} />
-          </Routes>
-        </main>
-
-      </BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
 
     </div>
   );
