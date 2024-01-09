233e2fa1370f686aaa8fecf329196cc584ed3b71
NicolasAndrehh <nicolasolaya22@gmail.com>
2024-01-08 23:26:50 -0500

    Improve about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,14 +1,9 @@
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import microverseCertificate from '../../downloads/microverse-certificate.pdf';
-import senaDegree from '../../downloads/sena-degree.pdf';
 import cssLogo from '../../img/css-logo.png';
 import gitLogo from '../../img/git-logo.png';
 import htmlLogo from '../../img/html-logo.png';
 import javascriptLogo from '../../img/javascript-logo.png';
 import laravelLogo from '../../img/laravel-logo.png';
-import locationIcon from '../../img/location-icon.png';
 import microverseLogo from '../../img/microverse-logo.png';
 import mysqlLogo from '../../img/mysql-logo.png';
 import phpLogo from '../../img/php-logo.png';
@@ -20,6 +15,7 @@ import reduxLogo from '../../img/redux-logo.png';
 import rubyLogo from '../../img/ruby-logo.png';
 import senaLogo from '../../img/sena-logo.jpg';
 import othersIcon from '../../img/tools-icon.png';
+import zeroTwo from '../../img/zero-two.png';
 import './about.scss';
 
 const skills = [
@@ -39,38 +35,131 @@ const skills = [
 
 const About = () => (
   <div id="about" className="about-page">
+    <section className="experience-education-section">
+      <h3 className="title">
+        Experience and education
+      </h3>
+      <div className="education-container">
+        <div className="timeline">
+          <span className="date">Now</span>
+          <span className="circle" />
+          <hr className="line" />
+        </div>
+        <div className="education-card">
+          <img src={microverseLogo} alt="Microverse logo" className="education-logo microverse-logo" />
+          <h4>Full-stack Web Development</h4>
+          <small className="date">Jan 2023 - Aug 2023</small>
+          <p className="education-description">
+            Accumulated over 1300 hours dedicated to mastering algorithms, data structures,
+            and full-stack development. Simultaneously, I crafted projects using Ruby, Rails,
+            JavaScript, React, and Redux. Gained expertise in remote pair programming through
+            GitHub, employing industry-standard git-flow and participating in daily standups for
+            seamless communication and collaboration with international remote developers.
+          </p>
+        </div>
+      </div>
+      <div className="experience-container">
+        <div className="timeline">
+          <span className="date">2023</span>
+          <span className="circle" />
+          <hr className="line-professional" />
+          <span className="professional-highlight">Professional Experience</span>
+        </div>
+        <div className="experience-card">
+          <img src={rappiLogo} alt="Rappi company logo" className="company-logo" />
+          <div className="card-info">
+            <h4>Back-end Developer</h4>
+            <p className="date">
+              July 2022 - July 2023 (1 Year)
+            </p>
+            <p className="experience-description">
+              Collaborated with senior developers to design
+              and implement RESTful APIs using
+              {' '}
+              <span className="highlight">Spring Boot</span>
+              {' '}
+              and
+              {' '}
+              <span className="highlight">Kotlin</span>
+              . Notably,
+              I contributed to creating the
+              {' '}
+              authentication and authorization endpoint for the About Rappi page.
+              Managing HTTP
+              {' '}
+              <span className="highlight">requests / responses</span>
+              {' '}
+              and handling
+              {' '}
+              <span className="highlight">exceptions / errors</span>
+              {' '}
+              refined my backend development skills
+              and provided insights into maintaining a robust tech ecosystem at Rappi.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="education-container">
+        <div className="timeline">
+          <span className="date">2022</span>
+          <span className="circle" />
+          <hr className="line" />
+        </div>
+        <div className="education-card">
+          <img src={senaLogo} alt="Sena logo" className="education-logo sena-logo" />
+          <h4>Software Analysis and Development</h4>
+          <small className="date">Jan 2021 - June 2022</small>
+          <p className="education-description">
+            Introduced to programming here, I built a strong base for a software analyst
+            and developer role. This career involves mastering software application tasks
+            like analysis, design, development, and maintenance. Proficiency in programming
+            languages, collaboration, and crafting efficient solutions are crucial.
+            In today&apos;s tech-focused world, software analysis and development
+            offer substantial growth and innovation opportunities.
+          </p>
+        </div>
+      </div>
+    </section>
+    <hr className="separator" />
     <section className="about-section">
       <h2 className="title">About me.</h2>
-
-      <p>
-        I&apos;m a passionate Full-stack Developer dedicated to crafting exceptional digital
-        experiences.
-        <br />
-        My expertise spans from React-powered front-ends to Ruby on Rails-driven back-ends,
-        where I specialize in creating robust APIs.
-        <br />
-        Excelling in both realms, I seamlessly unite aesthetics and functionality,
-        translating complexity into elegance.
-      </p>
-
-      <h4>Some interesting things about me:</h4>
-      <ul>
-        <li>
-          I take great pleasure in upholding a healthy lifestyle and regularly hitting the gym.
-        </li>
-        <li>
-          Anime totally captures my interest, and I really enjoy checking
-          out a diverse range of genres it has to offer.
-        </li>
-        <li>At the age of 20, I&apos;m embarking on a journey filled with learning and growth.</li>
-        <li>
-          My native language is
+      <div className="about-group">
+        <p>
+          👋 Hey there! I&apos;m Nicolas Olaya, a Full Stack Web Developer with
           {' '}
-          <span className="strong">Spanish</span>
-          , and I&apos;ve also honed my English skills to a solid upper-intermediate level.
-        </li>
-      </ul>
-
+          <span className="highlight">1 year of hands-on experience.</span>
+        </p>
+        <p>
+          Specializing in
+          {' '}
+          <span className="highlight">React</span>
+          {' '}
+          for dynamic front-end experiences and
+          {' '}
+          <span className="highlight">Ruby on Rails </span>
+          {' '}
+          for
+          robust back-end solutions and proficient in
+          {' '}
+          <span className="highlight">SQL</span>
+          {' '}
+          for effective database management.
+        </p>
+        <p>
+          I&apos;ve had the privilege of contributing to diverse projects,
+          ensuring both
+          {' '}
+          <span className="highlight">functionality</span>
+          {' '}
+          and a great
+          {' '}
+          <span className="highlight">user experience.</span>
+          {' '}
+          Let&apos;s connect and explore how my skills can elevate your next
+          web development endeavor!
+        </p>
+        <img src={zeroTwo} alt="Sakura Chiyo" />
+      </div>
     </section>
     <hr className="separator" />
     <section className="skills-section">
@@ -145,129 +234,6 @@ const About = () => (
       </div>
     </section>
     <hr className="separator" />
-    <section className="experience-section">
-      <h3 className="title">
-        My working
-        <br />
-        experience
-      </h3>
-      <div className="experience-container">
-        <div className="experience-card">
-          <img src={rappiLogo} alt="Rappi company logo" className="company-logo" />
-          <div className="card-header">
-            <h4>Back-end Developer</h4>
-            <p className="experience-description">
-              During my internship at Rappi as a Back End Developer,
-              I focused on designing and implementing RESTful APIs using Spring Boot.
-              I was responsible for managing HTTP requests and responses, as well as effectively
-              handling exceptions and errors in API responses.
-              This experience strengthened my skills in backend development and provided valuable
-              insights into maintaining a robust and user-friendly tech ecosystem at Rappi.
-            </p>
-          </div>
-          <div className="card-details">
-            <div className="responsabilities">
-              <h5>Responsabilities</h5>
-              <ul>
-                <li>Designing and implementing RESTful APIs using Spring Boot.</li>
-                <li>Handling HTTP requests and responses.</li>
-                <li>Handling exceptions and errors in API responses.</li>
-              </ul>
-            </div>
-            <div className="abilities-learned">
-              <h5>Abilities learned</h5>
-              <p>
-                I&apos;ve developed strong teamwork and collaboration skills, mastering effective
-                communication and adaptability. These soft skills have shown me the importance
-                of diverse perspectives and leveraging individual strengths for shared success.
-              </p>
-            </div>
-          </div>
-          <div className="card-footer">
-            <p className="location">
-              <img src={locationIcon} alt="Location icon" />
-              Colombia, Remote
-            </p>
-            <p className="date">
-              Jul 2022 - Jan 2023
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <hr className="separator" />
-    <section className="education-section">
-      <h3 className="title">My education</h3>
-      <div className="education-container">
-        <div className="education-card">
-          <img src={microverseLogo} alt="Microverse logo" className="education-logo microverse-logo" />
-          <div className="card-info">
-            <div className="card-header">
-              <h4>Full Stack Web Development</h4>
-              <small className="end-date">01 August 2023</small>
-            </div>
-            <div className="card-body">
-              <p className="description">
-                Accumulated over 1300 hours dedicated to mastering algorithms, data structures,
-                and full-stack development. Simultaneously, I crafted projects using Ruby, Rails,
-                JavaScript, React, and Redux. Gained expertise in remote pair programming through
-                GitHub, employing industry-standard git-flow and participating in daily standups for
-                seamless communication and collaboration with international remote developers.
-              </p>
-              <div className="education-skills">
-                <p>
-                  <span className="strong">Skills:</span>
-                  {' '}
-                  English, communication, collaboration
-                </p>
-                <p>
-                  <span className="strong">Stack:</span>
-                  {' '}
-                  React, Redux, Sass, Ruby, Ruby on Rails, PostgreSQL
-                </p>
-              </div>
-            </div>
-          </div>
-          <a href={microverseCertificate} className="download-certificate microverse" download="Nicolas microverse certificate">
-            <FontAwesomeIcon icon={faFile} className="icon" />
-          </a>
-        </div>
-        <div className="education-card">
-          <img src={senaLogo} alt="Sena logo" className="education-logo sena-logo" />
-          <div className="card-info">
-            <div className="card-header">
-              <h4>Software analyst and developer</h4>
-              <small className="end-date">01 January 2023</small>
-            </div>
-            <div className="card-body">
-              <p className="description">
-                Introduced to programming here, I built a strong base for a software analyst
-                and developer role. This career involves mastering software application tasks
-                like analysis, design, development, and maintenance. Proficiency in programming
-                languages, collaboration, and crafting efficient solutions are crucial.
-                In today&apos;s tech-focused world, software analysis and development
-                offer substantial growth and innovation opportunities.
-              </p>
-              <div className="education-skills">
-                <p>
-                  <span className="strong">Skills:</span>
-                  {' '}
-                  Agile Methodologies, Database Design, Systems Development Life Cycle (SDLC)
-                </p>
-                <p>
-                  <span className="strong">Stack:</span>
-                  {' '}
-                  HTML, CSS, Javascript, PHP, Laravel, MySQL
-                </p>
-              </div>
-            </div>
-          </div>
-          <a href={senaDegree} className="download-certificate sena" download="Nicolas sena degree">
-            <FontAwesomeIcon icon={faFile} className="icon" />
-          </a>
-        </div>
-      </div>
-    </section>
   </div>
 );
 
